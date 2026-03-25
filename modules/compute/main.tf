9e75b9ee7e1a207577fdda6dfb981cab49a08c89
Deepak2705-cpu <124522728+Deepak2705-cpu@users.noreply.github.com>
2026-03-25 18:33:45 +0530

    Add Azure resources for VM deployment

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -1,1 +1,63 @@
+resource "azurerm_public_ip" "main" {
+  name                = "${var.environment}-vm-pip"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
 
+  tags = {
+    environment = var.environment
+    managed_by  = "Terraform"
+  }
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.environment}-vm-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.main.id
+  }
+
+  tags = {
+    environment = var.environment
+    managed_by  = "Terraform"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "main" {
+  name                = "${var.environment}-vm"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.main.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = 30
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+
+  tags = {
+    environment = var.environment
+    managed_by  = "Terraform"
+  }
+}
