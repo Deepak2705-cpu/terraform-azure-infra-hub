603e2b289af4a997c82858b33caee61f336b3770
Deepak2705-cpu <124522728+Deepak2705-cpu@users.noreply.github.com>
2026-03-25 18:29:46 +0530

    Add output variables for various resources

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,19 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
+}
 
+output "vnet_id" {
+  value = module.networking.vnet_id
+}
+
+output "subnet_id" {
+  value = module.networking.subnet_id
+}
+
+output "vm_private_ip" {
+  value = module.compute.vm_private_ip
+}
+
+output "storage_account_name" {
+  value = module.storage.storage_account_name
+}
