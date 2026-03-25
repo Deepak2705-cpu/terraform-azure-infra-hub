eb1f0fd33c53e4ee8434125ebe47e9030d9934f6
Deepak2705-cpu <124522728+Deepak2705-cpu@users.noreply.github.com>
2026-03-25 18:36:13 +0530

    Add outputs for VM ID and IP addresses

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,1 +1,11 @@
+output "vm_id" {
+  value = azurerm_linux_virtual_machine.main.id
+}
 
+output "vm_private_ip" {
+  value = azurerm_network_interface.main.private_ip_address
+}
+
+output "vm_public_ip" {
+  value = azurerm_public_ip.main.ip_address
+}
