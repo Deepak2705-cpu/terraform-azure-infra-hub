6898bcd95331216d70e3b3bcb054e40004cd59fe
Deepak2705-cpu <124522728+Deepak2705-cpu@users.noreply.github.com>
2026-03-25 18:31:55 +0530

    Add variables for networking module configuration

--- modules/networking/variable.tf
+++ modules/networking/variable.tf
@@ -1,1 +1,5 @@
-
+variable "resource_group_name" { type = string }
+variable "location"            { type = string }
+variable "environment"         { type = string }
+variable "vnet_address_space"  { type = string }
+variable "subnet_prefixes"     { type = map(string) }
