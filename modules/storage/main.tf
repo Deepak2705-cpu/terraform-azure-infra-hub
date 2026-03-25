52076a3e7df005c5cec5d3a52c1b129933e96975
Deepak2705-cpu <124522728+Deepak2705-cpu@users.noreply.github.com>
2026-03-25 18:42:46 +0530

    Add Azure Storage Account and Containers

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -1,1 +1,32 @@
+resource "azurerm_storage_account" "main" {
+  name                     = "${var.environment}deepakinfra"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
+  blob_properties {
+    versioning_enabled = true
+
+    delete_retention_policy {
+      days = 7
+    }
+  }
+
+  tags = {
+    environment = var.environment
+    managed_by  = "Terraform"
+  }
+}
+
+resource "azurerm_storage_container" "app_data" {
+  name                  = "app-data"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "logs" {
+  name                  = "logs"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
+}
