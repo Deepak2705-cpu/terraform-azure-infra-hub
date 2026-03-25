41c5bbbb0757845c583e5803a19bf57a1f7026bc
Deepak2705-cpu <124522728+Deepak2705-cpu@users.noreply.github.com>
2026-03-25 18:32:54 +0530

    Add outputs for virtual network and subnets

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -1,1 +1,19 @@
+output "vnet_id" {
+  value = azurerm_virtual_network.main.id
+}
 
+output "vnet_name" {
+  value = azurerm_virtual_network.main.name
+}
+
+output "subnet_id" {
+  value = azurerm_subnet.subnets["app"].id
+}
+
+output "subnet_ids" {
+  value = { for k, v in azurerm_subnet.subnets : k => v.id }
+}
+
+output "nsg_id" {
+  value = azurerm_network_security_group.main.id
+}
